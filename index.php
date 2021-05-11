8c2160136bc1fd41a3e58599304720ab2c74677a
mr why <3mrwhy@gmail.com>
2021-05-11 22:20:52 +0530

    updated changes

--- index.php
+++ index.php
@@ -1,33 +1,50 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<header>
+		<div class="main-container">
+			<div class="inner-container"></div>
+		</div>
+	</header>
+	<footer>
+		<div>
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</div>
+		<div>
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</div>
+		<div>
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</div>
+		<div>
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</div>
+	</footer>
 
-/*
-
-	This is the databse class and perform all the operation of the crud
- 
-	class name: Database
-
- */ 
 
 
-	class Database 
-	{
-		private $con;   						// connection variable
-		private $server 	= "localhost";   	// server name
-		private $username 	= "root";   		// username
-		private $pass 		= "";   			// password
-		private $database 	= "dummy";   		// database name
-
-
-		function __construct()
-		{
-			// Here we are establishing  connection with databse
-			$this->con = new mysqli($this->server, $this->username, $this->pass, $this->database);
-			if($this->con) 
-			{
-				echo "connected"; 
-			} else {
-				echo "not connected";
-			}
-		}
-	}
-?>
+</body>
+</html>
